4dd3b94b57e97a7316106b9ebd5b6d86f87dbd91
lucasdellar <22032525+lucasdellar@users.noreply.github.com>
2018-06-10 19:23:00 -0300

    Avances con ABM Usuario

---
+++ FrbaHotel/AbmUsuario/AltaUsuario.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrbaHotel.AbmUsuario
+{
+    public partial class AltaUsuario : Form
+    {
+        private Label lblUsername;
+        private Label lblPassword;
+        private DateTimePicker dtFN;
+        private Label lblRol;
+        private Label lblNombre;
+        private Label lblApellido;
+        private Label lblTipo;
+        private Label lblNId;
+        private Label lblEmail;
+        private Label lblTel;
+        private Label lblFN;
+        private Label lblCalle;
+        private Label lblLocalidad;
+        private TextBox txtUsername;
+        private TextBox txtNombre;
+        private ComboBox cboRol;
+        private TextBox txtApellido;
+        private TextBox txtNroId;
+        private TextBox txtEmail;
+        private TextBox txtCalle;
+        private TextBox txtLocalidad;
+        private TextBox txtTel;
+        private ComboBox cboTipoId;
+        private MaskedTextBox mtxtPassword;
+    
+        public AltaUsuario()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.dtFN = new System.Windows.Forms.DateTimePicker();
+            this.mtxtPassword = new System.Windows.Forms.MaskedTextBox();
+            this.lblRol = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.lblNId = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblTel = new System.Windows.Forms.Label();
+            this.lblFN = new System.Windows.Forms.Label();
+            this.lblCalle = new System.Windows.Forms.Label();
+            this.lblLocalidad = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.cboRol = new System.Windows.Forms.ComboBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtNroId = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtCalle = new System.Windows.Forms.TextBox();
+            this.txtLocalidad = new System.Windows.Forms.TextBox();
+            this.txtTel = new System.Windows.Forms.TextBox();
+            this.cboTipoId = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblUsername.Location = new System.Drawing.Point(35, 30);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(73, 17);
+            this.lblUsername.TabIndex = 0;
+            this.lblUsername.Text = "Username";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblPassword.Location = new System.Drawing.Point(35, 60);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(69, 17);
+            this.lblPassword.TabIndex = 1;
+            this.lblPassword.Text = "Password";
+            // 
+            // dtFN
+            // 
+            this.dtFN.Location = new System.Drawing.Point(205, 240);
+            this.dtFN.Name = "dtFN";
+            this.dtFN.Size = new System.Drawing.Size(200, 20);
+            this.dtFN.TabIndex = 2;
+            // 
+            // mtxtPassword
+            // 
+            this.mtxtPassword.Location = new System.Drawing.Point(205, 60);
+            this.mtxtPassword.Name = "mtxtPassword";
+            this.mtxtPassword.Size = new System.Drawing.Size(100, 20);
+            this.mtxtPassword.TabIndex = 3;
+            // 
+            // lblRol
+            // 
+            this.lblRol.AutoSize = true;
+            this.lblRol.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblRol.Location = new System.Drawing.Point(35, 90);
+            this.lblRol.Name = "lblRol";
+            this.lblRol.Size = new System.Drawing.Size(92, 17);
+            this.lblRol.TabIndex = 4;
+            this.lblRol.Text = "Rol Asignado";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblNombre.Location = new System.Drawing.Point(35, 120);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(58, 17);
+            this.lblNombre.TabIndex = 5;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblApellido.Location = new System.Drawing.Point(35, 150);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(58, 17);
+            this.lblApellido.TabIndex = 6;
+            this.lblApellido.Text = "Apellido";
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblTipo.Location = new System.Drawing.Point(35, 180);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(142, 17);
+            this.lblTipo.TabIndex = 7;
+            this.lblTipo.Text = "Tipo de Identificación";
+            // 
+            // lblNId
+            // 
+            this.lblNId.AutoSize = true;
+            this.lblNId.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblNId.Location = new System.Drawing.Point(35, 210);
+            this.lblNId.Name = "lblNId";
+            this.lblNId.Size = new System.Drawing.Size(141, 17);
+            this.lblNId.TabIndex = 8;
+            this.lblNId.Text = "Nro. de Identificación";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblEmail.Location = new System.Drawing.Point(35, 300);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(42, 17);
+            this.lblEmail.TabIndex = 9;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblTel
+            // 
+            this.lblTel.AutoSize = true;
+            this.lblTel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblTel.Location = new System.Drawing.Point(35, 270);
+            this.lblTel.Name = "lblTel";
+            this.lblTel.Size = new System.Drawing.Size(64, 17);
+            this.lblTel.TabIndex = 10;
+            this.lblTel.Text = "Teléfono";
+            // 
+            // lblFN
+            // 
+            this.lblFN.AutoSize = true;
+            this.lblFN.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblFN.Location = new System.Drawing.Point(35, 240);
+            this.lblFN.Name = "lblFN";
+            this.lblFN.Size = new System.Drawing.Size(141, 17);
+            this.lblFN.TabIndex = 11;
+            this.lblFN.Text = "Fecha de Nacimiento";
+            // 
+            // lblCalle
+            // 
+            this.lblCalle.AutoSize = true;
+            this.lblCalle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblCalle.Location = new System.Drawing.Point(35, 330);
+            this.lblCalle.Name = "lblCalle";
+            this.lblCalle.Size = new System.Drawing.Size(39, 17);
+            this.lblCalle.TabIndex = 12;
+            this.lblCalle.Text = "Calle";
+            // 
+            // lblLocalidad
+            // 
+            this.lblLocalidad.AutoSize = true;
+            this.lblLocalidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblLocalidad.Location = new System.Drawing.Point(35, 360);
+            this.lblLocalidad.Name = "lblLocalidad";
+            this.lblLocalidad.Size = new System.Drawing.Size(69, 17);
+            this.lblLocalidad.TabIndex = 13;
+            this.lblLocalidad.Text = "Localidad";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(205, 30);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtUsername.TabIndex = 14;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(205, 120);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 15;
+            // 
+            // cboRol
+            // 
+            this.cboRol.FormattingEnabled = true;
+            this.cboRol.Location = new System.Drawing.Point(205, 90);
+            this.cboRol.Name = "cboRol";
+            this.cboRol.Size = new System.Drawing.Size(121, 21);
+            this.cboRol.TabIndex = 16;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(205, 150);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 17;
+            // 
+            // txtNroId
+            // 
+            this.txtNroId.Location = new System.Drawing.Point(205, 210);
+            this.txtNroId.Name = "txtNroId";
+            this.txtNroId.Size = new System.Drawing.Size(100, 20);
+            this.txtNroId.TabIndex = 18;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(205, 300);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 19;
+            // 
+            // txtCalle
+            // 
+            this.txtCalle.Location = new System.Drawing.Point(205, 330);
+            this.txtCalle.Name = "txtCalle";
+            this.txtCalle.Size = new System.Drawing.Size(100, 20);
+            this.txtCalle.TabIndex = 20;
+            // 
+            // txtLocalidad
+            // 
+            this.txtLocalidad.Location = new System.Drawing.Point(205, 360);
+            this.txtLocalidad.Name = "txtLocalidad";
+            this.txtLocalidad.Size = new System.Drawing.Size(100, 20);
+            this.txtLocalidad.TabIndex = 21;
+            // 
+            // txtTel
+            // 
+            this.txtTel.Location = new System.Drawing.Point(205, 270);
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(100, 20);
+            this.txtTel.TabIndex = 22;
+            // 
+            // cboTipoId
+            // 
+            this.cboTipoId.FormattingEnabled = true;
+            this.cboTipoId.Location = new System.Drawing.Point(205, 180);
+            this.cboTipoId.Name = "cboTipoId";
+            this.cboTipoId.Size = new System.Drawing.Size(121, 21);
+            this.cboTipoId.TabIndex = 23;
+            // 
+            // AltaUsuario
+            // 
+            this.ClientSize = new System.Drawing.Size(434, 411);
+            this.Controls.Add(this.cboTipoId);
+            this.Controls.Add(this.txtTel);
+            this.Controls.Add(this.txtLocalidad);
+            this.Controls.Add(this.txtCalle);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNroId);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.cboRol);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lblLocalidad);
+            this.Controls.Add(this.lblCalle);
+            this.Controls.Add(this.lblFN);
+            this.Controls.Add(this.lblTel);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblNId);
+            this.Controls.Add(this.lblTipo);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblRol);
+            this.Controls.Add(this.mtxtPassword);
+            this.Controls.Add(this.dtFN);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Name = "AltaUsuario";
+            this.Text = "Alta de Usuario";
+            this.Load += new System.EventHandler(this.AltaUsuario_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void AltaUsuario_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ FrbaHotel/AbmUsuario/SeleccionUsuario.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrbaHotel.AbmUsuario
+{
+    public partial class SeleccionUsuario : Form
+    {
+        private DataGridView dgCustomer;
+        private GroupBox gboFiltros;
+        private Button btnLimpiar;
+        private Button btnBuscar;
+        private TextBox txtEmail;
+        private Label lblEmail;
+        private TextBox txtNroId;
+        private Label lblNroId;
+        private ComboBox cboTipoId;
+        private Label lblTipo;
+        private TextBox txtApellido;
+        private Label lblApellido;
+        private TextBox txtNombre;
+        private ComboBox comboBox2;
+        private Label label1;
+        private Label lblNombre;
+    
+        public SeleccionUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.dgCustomer = new System.Windows.Forms.DataGridView();
+            this.gboFiltros = new System.Windows.Forms.GroupBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtNroId = new System.Windows.Forms.TextBox();
+            this.lblNroId = new System.Windows.Forms.Label();
+            this.cboTipoId = new System.Windows.Forms.ComboBox();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgCustomer)).BeginInit();
+            this.gboFiltros.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgCustomer
+            // 
+            this.dgCustomer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgCustomer.Location = new System.Drawing.Point(35, 250);
+            this.dgCustomer.Name = "dgCustomer";
+            this.dgCustomer.Size = new System.Drawing.Size(650, 200);
+            this.dgCustomer.TabIndex = 3;
+            // 
+            // gboFiltros
+            // 
+            this.gboFiltros.Controls.Add(this.comboBox2);
+            this.gboFiltros.Controls.Add(this.label1);
+            this.gboFiltros.Controls.Add(this.btnLimpiar);
+            this.gboFiltros.Controls.Add(this.btnBuscar);
+            this.gboFiltros.Controls.Add(this.txtEmail);
+            this.gboFiltros.Controls.Add(this.lblEmail);
+            this.gboFiltros.Controls.Add(this.txtNroId);
+            this.gboFiltros.Controls.Add(this.lblNroId);
+            this.gboFiltros.Controls.Add(this.cboTipoId);
+            this.gboFiltros.Controls.Add(this.lblTipo);
+            this.gboFiltros.Controls.Add(this.txtApellido);
+            this.gboFiltros.Controls.Add(this.lblApellido);
+            this.gboFiltros.Controls.Add(this.txtNombre);
+            this.gboFiltros.Controls.Add(this.lblNombre);
+            this.gboFiltros.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.gboFiltros.Location = new System.Drawing.Point(35, 30);
+            this.gboFiltros.Name = "gboFiltros";
+            this.gboFiltros.Size = new System.Drawing.Size(650, 200);
+            this.gboFiltros.TabIndex = 2;
+            this.gboFiltros.TabStop = false;
+            this.gboFiltros.Text = "Filtros de búsqueda";
+            this.gboFiltros.Enter += new System.EventHandler(this.gboFiltros_Enter);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(100, 135);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(150, 50);
+            this.btnLimpiar.TabIndex = 11;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(380, 135);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(150, 50);
+            this.btnBuscar.TabIndex = 10;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(115, 90);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(125, 23);
+            this.txtEmail.TabIndex = 9;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(35, 90);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(42, 17);
+            this.lblEmail.TabIndex = 8;
+            this.lblEmail.Text = "Email";
+            // 
+            // txtNroId
+            // 
+            this.txtNroId.Location = new System.Drawing.Point(420, 60);
+            this.txtNroId.Name = "txtNroId";
+            this.txtNroId.Size = new System.Drawing.Size(125, 23);
+            this.txtNroId.TabIndex = 7;
+            // 
+            // lblNroId
+            // 
+            this.lblNroId.AutoSize = true;
+            this.lblNroId.Location = new System.Drawing.Point(270, 60);
+            this.lblNroId.Name = "lblNroId";
+            this.lblNroId.Size = new System.Drawing.Size(141, 17);
+            this.lblNroId.TabIndex = 6;
+            this.lblNroId.Text = "Nro. de Identificación";
+            this.lblNroId.Click += new System.EventHandler(this.lblNro_Click);
+            // 
+            // cboTipoId
+            // 
+            this.cboTipoId.FormattingEnabled = true;
+            this.cboTipoId.Location = new System.Drawing.Point(420, 30);
+            this.cboTipoId.Name = "cboTipoId";
+            this.cboTipoId.Size = new System.Drawing.Size(125, 24);
+            this.cboTipoId.TabIndex = 5;
+            this.cboTipoId.Text = "Seleccionar";
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Location = new System.Drawing.Point(270, 30);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(142, 17);
+            this.lblTipo.TabIndex = 4;
+            this.lblTipo.Text = "Tipo de Identificación";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(115, 60);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(125, 23);
+            this.txtApellido.TabIndex = 3;
+            this.txtApellido.TextChanged += new System.EventHandler(this.txtApellido_TextChanged);
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(35, 60);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(58, 17);
+            this.lblApellido.TabIndex = 2;
+            this.lblApellido.Text = "Apellido";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(115, 30);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(125, 23);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(35, 30);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(58, 17);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(420, 90);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(125, 24);
+            this.comboBox2.TabIndex = 13;
+            this.comboBox2.Text = "Seleccionar";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(270, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 17);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Rol Asignado";
+            // 
+            // SeleccionUsuario
+            // 
+            this.ClientSize = new System.Drawing.Size(734, 486);
+            this.Controls.Add(this.dgCustomer);
+            this.Controls.Add(this.gboFiltros);
+            this.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.Name = "SeleccionUsuario";
+            this.Text = "Seleccionar Usuario";
+            ((System.ComponentModel.ISupportInitialize)(this.dgCustomer)).EndInit();
+            this.gboFiltros.ResumeLayout(false);
+            this.gboFiltros.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        private void gboFiltros_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblNro_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtApellido_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
