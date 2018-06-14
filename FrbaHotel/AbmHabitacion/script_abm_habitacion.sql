ca5ab2a9488aa03f52eff4880b6cd9190c8526a9
eliasgabrielkim <eliasgabrielkim@gmail.com (none)>
2018-06-14 01:42:56 -0300

    Arreglos en ABM Habitacion y script_inicial, se agregan stored procedures de ABM Habitacion

--- FrbaHotel/AbmHabitacion/script_abm_habitacion.sql
+++ FrbaHotel/AbmHabitacion/script_abm_habitacion.sql
@@ -26,59 +26,104 @@ BEGIN
 	Descripcion,
 	Habilitado
 	FROM [MMEL].[Habitacion] hab   
-	WHERE   @Piso= piso and
-@VistaAlExterior =VistaAlExterior  and
- @NumeroHabitacion =  hab.NumeroHabitacion and
- @IdHotel = hab.IdHotel  
+	
+	WHERE   
+ (@Piso is NULL OR (@Piso = piso))and
+  (@VistaAlExterior is NULL OR           (@VistaAlExterior = VistaAlExterior))and
+ (@NumeroHabitacion is NULL OR (@NumeroHabitacion = hab.NumeroHabitacion)) and
+ (@IdHotel is NULL OR (@IdHotel = hab.IdHotel))  and
+(@IdTipoHabitacion is NULL OR( @IdTipoHabitacion = hab.idTipoHabitacion))
 END
+
 GO
 ALTER PROCEDURE [MMEL].[HabitacionesAlta]
-@IdTipoHabitacion int,
-                   @NumeroHabitacion int,
+					@IdTipoHabitacion int,
+                    @NumeroHabitacion int,
                     @IdHotel int,
-                   @Piso int,
-                   @VistaAlExterior char(1),
-                   @Descripcion nvarchar(80),
-                   @Habilitado bit = null
+                    @Piso int,
+                    @VistaAlExterior char(1),
+                    @Descripcion nvarchar(80),
+                    @Habilitado char(1),
+					@MESSAGE int OUTPUT
 AS
-SET NOCOUNT ON 
+    SET NOCOUNT ON 
 	SET XACT_ABORT ON  
-	
+	DECLARE @ExisteHotel int
+	DECLARE @ExisteTipoHabitacion int
+	SELECT @ExisteTipoHabitacion = idTipoHabitacion FROM mmel.TipoHabitacion where @IdTipoHabitacion=idTipoHabitacion
+	SELECT @ExisteHotel = hot.idHotel FROM mmel.Hotel hot where @IdHotel = hot.idHotel
+	DECLARE @AUX int
+	PRINT @ExisteHotel
+	PRINT @ExisteTipoHabitacion
+	print @AUX
+	IF @ExisteHotel != 0 AND @ExisteTipoHabitacion!=0  
+	BEGIN
+		if NOT exists (SELECT idHabitacion FROM mmel.Habitacion where @IdTipoHabitacion = [idTipoHabitacion] AND 
+	 @NumeroHabitacion = [NumeroHabitacion] AND 
+	 @IdHotel=[idHotel] AND  
+	 @Piso= [Piso] AND 
+	 @VistaAlExterior= [VistaAlExterior] AND
+	 @Habilitado = [Habilitado] AND
+	 @Descripcion = [Descripcion])
+	 BEGIN
+	SET @MESSAGE = 1
+	PRINT @MESSAGE
 	BEGIN TRAN
-	
 	INSERT INTO [MMEL].[Habitacion] ([idTipoHabitacion], [NumeroHabitacion],[idHotel], [Piso],[VistaAlExterior],[Descripcion],[Habilitado])
 	SELECT @IdTipoHabitacion,
                    @NumeroHabitacion ,
-                    @IdHotel ,
+                   @IdHotel ,
                    @Piso ,
                    @VistaAlExterior ,
                    @Descripcion ,
                    @Habilitado 
-	
-               
 	COMMIT
+	END
+	ELSE
+	BEGIN
+	SET @MESSAGE = 0
+	PRINT @MESSAGE
+	END
+	END
+	ELSE
+	BEGIN
+	SET @MESSAGE = 0
+	PRINT @MESSAGE
+	END
 GO
 
 
 GO
 ALTER PROCEDURE [MMEL].[HabitacionesBaja]
-@IdTipoHabitacion int,
-                   @NumeroHabitacion int,
+					@IdTipoHabitacion int,
+                    @NumeroHabitacion int,
                     @IdHotel int,
-                   @Piso int,
-                   @VistaAlExterior char(1),
-                   @Habilitado bit = null
+                    @Piso int,
+                    @VistaAlExterior char(1),
+				@MESSAGE int OUTPUT
+					
 AS
 SET NOCOUNT ON 
 	SET XACT_ABORT ON  
+	DECLARE @HabitacionABorrar int
+	SELECT  @HabitacionABorrar=h.idHabitacion FROM [MMEL].[Habitacion] h join [MMEL].[Reserva] r on h.idHabitacion=r.idHabitacion where @IdTipoHabitacion = [idTipoHabitacion] AND 
+	 @NumeroHabitacion = [NumeroHabitacion] AND @IdHotel=[idHotel] AND  @Piso= [Piso] AND @VistaAlExterior= [VistaAlExterior] 
+	PRINT @HabitacionABorrar 
 	
+	IF @HabitacionABorrar = NULL
+	BEGIN
 	BEGIN TRAN
-	
 	DELETE FROM [MMEL].[Habitacion] WHERE  @IdTipoHabitacion = [idTipoHabitacion] AND 
-	 @NumeroHabitacion = [NumeroHabitacion] AND @IdHotel=[idHotel] AND  @Piso= [Piso] AND @VistaAlExterior= [VistaAlExterior] AND @Habilitado =[Habilitado])
-	
-               
+	 @NumeroHabitacion = [NumeroHabitacion] AND @IdHotel=[idHotel] AND  @Piso= [Piso] AND @VistaAlExterior= [VistaAlExterior] 
+		SET @MESSAGE = 1
 	COMMIT
+	END
+	ELSE
+	BEGIN
+	SET @MESSAGE = 0
+	print @MESSAGE
+
+	END
 GO
 ALTER PROCEDURE [MMEL].[HabitacionesModificar]
 @IdTipoHabitacion int,
@@ -87,7 +132,7 @@ ALTER PROCEDURE [MMEL].[HabitacionesModificar]
                    @Piso int,
                    @VistaAlExterior char(1),
                    @Descripcion nvarchar(80),
-                   @Habilitado bit = null
+                   @Habilitado char(1)
 AS
 SET NOCOUNT ON 
 	SET XACT_ABORT ON  
