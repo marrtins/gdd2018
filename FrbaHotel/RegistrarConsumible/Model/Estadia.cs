ae3bc50961af5d53cb489011e17455a6f39dd768
lucasdellar <22032525+lucasdellar@users.noreply.github.com>
2018-06-26 00:05:52 -0300

    Avances en Registrar Consumible

---
+++ FrbaHotel/RegistrarConsumible/Model/Estadia.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrbaHotel.RegistrarConsumibles.Model
+{
+    public static class MensajesValidacion
+    {
+        public static string Requerido = "El campo es requerido";
+    }
+
+    [Serializable]
+    public class Estadia : INotifyPropertyChanged
+    {
+        private string codigoReserva;
+        private int idTipoRegimen;
+        private string nroHabitacion;
+        private string piso;
+        private DateTime fechaCheckIn;
+        private DateTime fechaCheckOut;
+
+        [CustomRequired]
+        [RegularExpression("^[0-9]+$", ErrorMessage = "El campo solo puede contener numeros.")]
+        public string CodigoReserva
+        {
+            get => codigoReserva;
+            set
+            {
+                codigoReserva = value;
+                InvokePropertyChanged("");
+            }
+        }
+
+        [CustomRequired]
+        [RegularExpression("^[0-9]+$", ErrorMessage = "El campo solo puede contener numeros.")]
+        public string NroHabitacion
+        {
+            get
+            {
+                return nroHabitacion;
+            }
+
+            set
+            {
+                nroHabitacion = value;
+                InvokePropertyChanged("");
+            }
+        }
+
+        [CustomRequired]
+        [RegularExpression("^[0-9]+$", ErrorMessage = "El campo solo puede contener numeros.")]
+        public string Piso
+        {
+            get
+            {
+                return piso;
+            }
+
+            set
+            {
+                piso = value;
+                InvokePropertyChanged("");
+            }
+        }
+
+        [CustomRequired]
+        public DateTime FechaCheckIn
+        {
+            get => fechaCheckIn;
+            set
+            {
+                fechaCheckIn = value;
+                InvokePropertyChanged("");
+            }
+        }
+
+        [CustomRequired]
+        public DateTime FechaCheckOut
+        {
+            get => fechaCheckOut;
+            set
+            {
+                fechaCheckOut = value;
+                InvokePropertyChanged("");
+            }
+        }
+
+        public int IdTipoRegimen{get => idTipoRegimen; set idTipoRegimen = value; }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void InvokePropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
